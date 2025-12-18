90dcb03ad101cdbf291fcf09649e3ef9969fee6e
cj12o <cj2004jee@gmail.com>
2025-12-18 23:02:46 +0530

    done 2

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,13 @@
 "use client";
+import { Suspense } from "react";
 import ProjectsExplorer from "@/components/projectExplorer";
 const ProjectsPage = () => {
   
   return (
     <div className="flex flex-col min-h-screen dark:bg-bgdark">
-    <ProjectsExplorer/>
+    <Suspense fallback={<div className="p-6">Loading projects…</div>}>
+      <ProjectsExplorer/>
+    </Suspense>
     </div>
   );
 };
